410233456e2a9be4a4255048ac6edf7e8b5de91f
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-08 21:20:53 +0530

    NEW:Programe to Print The Element of Arrys

---
+++ src/com/bridgelabz/PrintArraysElement.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class PrintArraysElement {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{10, 20, 30, 40, 50};
+        System.out.println("Printing The Arrays Elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
